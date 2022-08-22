2c3fe11fb88a3e520326cb37fb904341d13dcf45
Sergio Martinez <smartinez@koderbit.com>
2022-08-22 13:09:41 -0400

    search functionality mock data source, clean architecture, view binding

--- app/src/main/java/com/aptivist/pokedex/di/AppModule.kt
+++ app/src/main/java/com/aptivist/pokedex/di/AppModule.kt
@@ -1,13 +1,25 @@
 package com.aptivist.pokedex.di
 
+import com.aptivist.pokedex.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
 }
--- app/src/main/java/com/aptivist/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/aptivist/pokedex/ui/MainActivity.kt
@@ -1,16 +1,21 @@
-package com.aptivist.pokedex
+package com.aptivist.pokedex.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.viewModels
+import com.aptivist.pokedex.R
+import com.aptivist.pokedex.ui.pokemonlist.PokemonListViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel: PokemonListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }

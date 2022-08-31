d732089d196adfeebd58dc9c7777fb15b9e40dda
Diego Beltrán Maldonado <diego.beltranm.95@outlook.com>
2022-08-31 18:27:51 -0400

    Updated domain and UI for pokemonList

--- app/src/main/java/com/aptivist/pokedex/domain/IPokemonDataSource.kt
+++ app/src/main/java/com/aptivist/pokedex/domain/IPokemonDataSource.kt
@@ -1,9 +1,11 @@
 package com.aptivist.pokedex.domain
 
 import com.aptivist.pokedex.domain.pokemon.Pokemon
+import com.aptivist.pokedex.domain.pokemon.PokemonListItem
 
 interface IPokemonDataSource {
 
     suspend fun getPokemonByNameOrID(searchTerm: String) : Pokemon
 
+    suspend fun getPokemonList(offset : Int, limit : Int): List<PokemonListItem>{return emptyList()}
 }
---
+++ app/src/main/java/com/aptivist/pokedex/domain/pokemon/PokemonListItem.kt
@@ -0,0 +1,6 @@
+package com.aptivist.pokedex.domain.pokemon
+
+data class PokemonListItem (
+    val id: Int,
+    val name: String
+    )
